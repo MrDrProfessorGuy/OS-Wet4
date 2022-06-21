ecda692192b47e6fe08ca0ef28736153a38daf57
MrDrprofessorGuy <guycohen801@gmail.com>
2022-06-21 13:26:28 +0300

    a

--- test.cpp
+++ test.cpp
@@ -27,24 +27,21 @@ public:
         std::cout << "======================= Print =======================" << std::endl;
     
         int counter = 0;
-        for (BlockMetadata* iter = &list.head; iter != nullptr ; iter = (BlockMetadata*)((char*)iter + iter->size + METADATA_SIZE)) {
+        for (BlockMetadata* iter = &list.head; iter != nullptr ; iter = (BlockMetadata*)(((char*)iter )+ iter->size + METADATA_SIZE)) {
             std::cout << "----------- BlockMetaData ["<< counter <<"] -----------" << std::endl;
-            std::cout << "      -size= " << iter->size << " ||      -is_free= " << iter->is_free << std::endl;
-            std::cout << "      -next= " << iter->next << " ||      -prev= " << iter->prev << std::endl;
-            std::cout << "      -next_free= " << iter->next_free << " ||      -prev= " << iter->prev_free << std::endl;
+            std::cout << "      -size= " << iter->size << "     ||      -is_free= " << iter->is_free << std::endl;
+            std::cout << "      -next= " << iter->next << "     ||      -prev= " << iter->prev << std::endl;
+            std::cout << "      -next_free= " << iter->next_free << "       ||      -prev= " << iter->prev_free << std::endl;
             std::cout << "---------------------------------------" << std::endl;
+            counter++;
         }
         
-        std::cout << "num_free_blocks = " << _num_free_blocks() << std::endl;
-        std::cout << "num_free_bytes = " <<  _num_free_bytes() << std::endl;
-        std::cout << "num_allocated_blocks = " << _num_allocated_blocks()  << std::endl;
-        std::cout << "num_allocated_bytes = " <<  _num_allocated_bytes() << std::endl;
-        std::cout << "num_beta_bytes = " <<  _num_meta_data_bytes() << std::endl;
-        std::cout << "size_meta_data = " <<  _size_meta_data() << std::endl;
-        std::cout << std::endl;
+        
     }
     
     void test1(){
+        print();
+        printHeap();
         void* a = smalloc(10);
         print();
         printHeap();
