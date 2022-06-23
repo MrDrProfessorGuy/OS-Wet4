6dd3154292ef75a42e90575a51e38f56cf5b2727
MrDrprofessorGuy <guycohen801@gmail.com>
2022-06-23 18:31:09 +0300

    a

--- test.cpp
+++ test.cpp
@@ -445,7 +445,7 @@ public:
                 BlockMetadata* block = get_block(block_num);
                 cout << "   address: " << block << endl;
                 
-                BlockMetadata* new_block = (BlockMetadata*)srealloc(block-1, size);
+                BlockMetadata* new_block = (BlockMetadata*)srealloc(block+1, size);
                 cout << "   block["<<block_num<<"]: " << block << "   Relocated to" << new_block << endl;
     
             }
